caaafc2a06c92eac1f67ecaf9e80e3458131c3e3
Balázs Ákos <balazs.akos@student.ms.sapientia.ro>
2023-10-11 17:37:02 +0300

    lab03

--- lab03/main_03.cpp
+++ lab03/main_03.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "List.h"
+#include "Stack.h"
 
 int main() {
-    std::cout << "Hello, Lab_03" << std::endl;
+    /*List list;
+    list.insertFirst(17);
+    list.insertFirst(6);
+    list.insertFirst(2);
+    list.print();
+    list.insertFirst(7);
+    list.print();
+    list.remove(7, List::DeleteFlag::EQUAL);
+    list.print();*/
+
+    Stack stack;
+    stack.push(3);
+    stack.push(2);
+    stack.push(1);
+    stack.pop();
     return 0;
 }
